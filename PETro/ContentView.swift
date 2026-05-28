23f4f0ee0d270d3f0c2577167b81b266ae380e0c
Viktoria Karpenko <viktoria.a.karpenko@gmail.com>
2026-05-28 14:47:42 +0300

    Initial Commit

---
+++ PETro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PETro
+//
+//  Created by Viktoria Karpenko on 28.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PETro/PETroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PETroApp.swift
+//  PETro
+//
+//  Created by Viktoria Karpenko on 28.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct PETroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
